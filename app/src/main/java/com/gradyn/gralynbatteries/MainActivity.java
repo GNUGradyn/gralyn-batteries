7077e3b551a3902329817604bd0516221514f299
Gradyn Wursten <me@gradyn.com>
2022-11-04 22:09:17 -0600

    stuff

--- app/src/main/java/com/gradyn/gralynbatteries/MainActivity.java
+++ app/src/main/java/com/gradyn/gralynbatteries/MainActivity.java
@@ -1,10 +1,13 @@
 package com.gradyn.gralynbatteries;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -15,17 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
-        Configuration config = ConfigurationHelper.LoadConfiguration(preferences);
-        Switch batteryReportingSwitch = (Switch) findViewById(R.id.BatteryReportingSwitch);
-        EditText gralynApiRootTextView = (EditText) findViewById(R.id.GralynApiRootTextbox);
-        EditText accessCode = (EditText) findViewById(R.id.AccessCodeTextbox);
-
+        final SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        final Configuration config = ConfigurationHelper.LoadConfiguration(preferences);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Switch batteryReportingSwitch = (Switch) findViewById(R.id.BatteryReportingSwitch);
+        final EditText gralynApiRootTextView = (EditText) findViewById(R.id.GralynApiRootTextbox);
+        final EditText accessCodeRootTextView = (EditText) findViewById(R.id.AccessCodeTextbox);
+        final Button savebutton = (Button) findViewById(R.id.SaveButton);
+
         batteryReportingSwitch.setChecked(config.getBatteryReporting());
         gralynApiRootTextView.setText(config.getApiRoot());
-        accessCode.setText(config.getAccessCode());
+        accessCodeRootTextView.setText(config.getAccessCode());
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                config.setAccessCode(accessCodeRootTextView.getText().toString());
+                config.setBatteryReporting(batteryReportingSwitch.isChecked());
+                config.setApiRoot(gralynApiRootTextView.getText().toString());
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity);
+            }
+        });
     }
 }
