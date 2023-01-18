4b7b772f27bc30be815f5f0bff72f77c1bd80fe3
Gradyn Wursten <me@gradyn.com>
2023-01-18 14:27:41 -0700

    nobody will read this anyway lol

--- app/src/main/java/com/gradyn/gralynbatteries/BatteryReportWorker.java
+++ app/src/main/java/com/gradyn/gralynbatteries/BatteryReportWorker.java
@@ -1,8 +1,13 @@
 package com.gradyn.gralynbatteries;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -17,11 +22,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 import androidx.preference.PreferenceManager;
+import androidx.work.ForegroundInfo;
+import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import static android.content.Context.BATTERY_SERVICE;
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 import com.gradyn.gralynbatteries.Configuration.Configuration;
 import com.gradyn.gralynbatteries.Configuration.ConfigurationHelper;
@@ -44,6 +54,7 @@ public class BatteryReportWorker extends Worker {
     @Override
     public Result doWork() {
         Log.println(Log.INFO, "BatteryReportWorker", "Running battery report worker");
+        setForegroundAsync(createForegroundInfo());
         Configuration config = ConfigurationHelper.LoadConfiguration(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         BatteryManager bm = (BatteryManager) getApplicationContext().getSystemService(BATTERY_SERVICE);
         if (!config.getBatteryReporting()) return Result.success();
@@ -82,4 +93,39 @@ public class BatteryReportWorker extends Worker {
 
         return Result.success();
     }
+
+    @NonNull
+    private ForegroundInfo createForegroundInfo() {
+        // Build a notification using bytesRead and contentLength
+
+        Context context = getApplicationContext();
+        String id = "Battery Report Worker";
+        String title = "Sending Battery Report";
+        String cancel = "Cancel Report";
+        // This PendingIntent can be used to cancel the worker
+        PendingIntent intent = WorkManager.getInstance(context)
+                .createCancelPendingIntent(getId());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+        }
+
+        Notification notification = new NotificationCompat.Builder(context, id)
+                .setContentTitle(title)
+                .setTicker(title)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setOngoing(true)
+                // Add the cancel action to the notification which can
+                // be used to cancel the worker
+                .addAction(android.R.drawable.ic_delete, cancel, intent)
+                .build();
+
+        return new ForegroundInfo(1, notification);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        // Create a Notification channel
+    }
+
 }
